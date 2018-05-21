b296dde52542c1b936905c6c4674bcc71768c4b0
sonicrules11 <sonic886310@gmail.com>
2018-05-21 01:16:08 -0700

    Remove circle strafe

--- Hacks/bhop.h
+++ Hacks/bhop.h
@@ -2,4 +2,3 @@
 
 void DoBhop(CUserCmd* cmd, C_BaseEntity* local);
 void DoAutoStrafe(CUserCmd* cmd, C_BaseEntity* local);
-void CirlceStrafe(C_BaseEntity *localPlayer, CUserCmd *cmd, Vector &oldAngles);
